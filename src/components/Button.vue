c3afe857cd4585ac302ed365623a2d84374c6013
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-04-14 00:10:26 -0300

    creates button component

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button type="button">
+    {{name}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #54a9b1; 
+  color: white;
+  font-size: 12;
+  font-weight: 600;
+  border-radius: 5px;
+  height: 30px;
+  border: none;
+}
+button:hover {
+  cursor: pointer;
+  pointer-events: 'auto';
+  opacity: 0.8
+}
+</style>
