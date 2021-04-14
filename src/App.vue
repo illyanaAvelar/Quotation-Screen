57433e94aaa4364ff7a377bb4fcfa742078664d7
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-04-14 00:15:29 -0300

    adjusts button

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   background-color: #fafafa;
+  height: 100vh;
 }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -15,13 +15,15 @@ export default {
 
 <style scoped>
 button {
-  background-color: #54a9b1; 
+  background-color: #39a8b3; 
   color: white;
   font-size: 12;
   font-weight: 600;
   border-radius: 5px;
   height: 30px;
   border: none;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 button:hover {
   cursor: pointer;
