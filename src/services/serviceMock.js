b1fe0f522710e5f3e7aa751cfd1eba61804c2ad1
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-04-15 01:23:49 -0300

    adds service simulation (v-for bug must be fixed)

---
+++ src/services/serviceMock.js
@@ -0,0 +1,58 @@
+export const quotationsList = [
+  {
+    quotationId: "0010.96",
+    personName: "Mariana",
+    product: "porcas",
+    installment: "12x",
+    RCS: "2",
+    quotationEndDate: "22/01/2020",
+    createdAt: "22/01/2020",
+    destiny: "Goitacazes",
+    itemDescription: "Porc vag ferrovs n225p43ge",
+    amount: "120",
+    unit: "Peças",
+    neededAt: "11/12/2020"  
+  },
+  {
+    quotationId: "0010.97",
+    personName: "Mariana",
+    product: "porcas",
+    installment: "12x",
+    RCS: "2",
+    quotationEndDate: "22/01/2020",
+    createdAt: "22/01/2020",
+    destiny: "Goitacazes",
+    itemDescription: "Porc vag ferrovs n225p43ge",
+    amount: "120",
+    unit: "Peças",
+    neededAt: "11/12/2020"  
+  },
+  {
+    quotationId: "0010.98",
+    personName: "Mariana",
+    product: "porcas",
+    installment: "12x",
+    RCS: "2",
+    quotationEndDate: "22/01/2020",
+    createdAt: "22/01/2020",
+    destiny: "Goitacazes",
+    itemDescription: "Porc vag ferrovs n225p43ge",
+    amount: "120",
+    unit: "Peças",
+    neededAt: "11/12/2020"  
+  },
+  {
+    quotationId: "0010.99",
+    personName: "Mariana",
+    product: "porcas",
+    installment: "12x",
+    RCS: "2",
+    quotationEndDate: "22/01/2020",
+    createdAt: "22/01/2020",
+    destiny: "Goitacazes",
+    itemDescription: "Porc vag ferrovs n225p43ge",
+    amount: "120",
+    unit: "Peças",
+    neededAt: "11/12/2020"  
+  }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div>
     <Header/>
     <QuotationInfo/>
+    <!-- v-for não está renderizando -->
+    <!-- <tr v-for="quotation of quotations" :key="quotation.quotationId">
+      <ItemCard quote={{ quotation }}/>
+    </tr> -->
     <ItemCard/>
     <ItemCard/>
-    <ItemCard/> 
+    <ItemCard/>
     <ItemCard/>
   </div>
 </template>
@@ -14,8 +18,22 @@
 import Header from '../components/Header.vue'
 import QuotationInfo from '../components/QuotationInfo.vue'
 import ItemCard from '../components/ItemCard.vue'
+import Quotations from '../services/quotations'
 
 export default {
+ 
+ data () {
+    return {
+      quotations: []
+    }
+  },
+
+  created () {
+    Quotations.list().then(response => {
+      this.quotations = response.data
+    })
+  },
+  
   name: 'App',
   components: {
     Header,
